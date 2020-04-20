1a4a46ceb7dc20cae77554bb643870bc2dcdf255
Kristoffer Alquiza <kalquiza@gmail.com>
2020-04-19 20:41:02 -0700

    Configure express server

---
+++ src/server/index.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const express = require('express')
+const mockAPIResponse = require('./mockAPI.js')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express()
+app.use(cors())
+// to use json
+app.use(bodyParser.json())
+// to use url encoded values
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+app.use(express.static('dist'))
+
+console.log(__dirname)
+
+app.get('/', function (req, res) {
+    // res.sendFile('dist/index.html')
+    res.sendFile(path.resolve('src/client/views/index.html'))
+})
+
+// designates what port the app will listen to for incoming requests
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081!')
+})
+
+// GET test tesponse from mock api
+app.get('/test', function (req, res) {
+    res.send(mockAPIResponse)
+})
