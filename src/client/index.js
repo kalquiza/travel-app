e8ee3519bf1208dfb047dfc6991beefb00148680
Kristoffer Alquiza <kalquiza@gmail.com>
2020-04-22 23:55:30 -0700

    Update function imports

--- src/client/index.js
+++ src/client/index.js
@@ -1,15 +1,9 @@
 // import { checkForName } from './js/nameChecker'
-import { postData } from './js/application'
-import { getDestination } from './js/application'
 import { performAction } from './js/application'
-import { updateUI } from './js/application'
 
 import './styles/resets.scss'
 import './styles/styles.scss'
 
 export {
-    postData,
-    getDestination,
-    performAction,
-    updateUI
+    performAction
 }
